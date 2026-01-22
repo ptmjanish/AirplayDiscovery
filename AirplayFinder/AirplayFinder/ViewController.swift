ed6aa0242dd292d8580f626a7445457e1123543a
Janish <ptmjanish@gmail.com>
2026-01-22 10:49:39 +0530

    Initial Setup

---
+++ AirplayFinder/AirplayFinder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AirplayFinder
+//
+//  Created by Mohammed Janish on 22/01/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
